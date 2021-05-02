dbce07ae7ec70a726168f665425cf39e7e90df60
Oriana <oriana.nohemi22@gmail.com>
2021-05-02 12:00:17 -0500

    Definir tema, agrega imagenes del proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import Router from './router/Router';
 import './App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#222053"
+    },
+    secondary: {
+      main: "#76ec9a"
+    }
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router />
+    </ThemeProvider>
   );
 }
 
