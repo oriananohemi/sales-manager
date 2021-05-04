03347540c9c9d4d5807d891f6de95213c9f43914
Oriana <oriana.nohemi22@gmail.com>
2021-05-04 10:38:51 -0500

    estilos y estructura

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(( ) => ({
     image: {
         marginRight: "auto",
         width: 200,
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,53 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+import { Link as RouterLink } from 'react-router-dom';
+import { Container } from '@material-ui/core';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Link from '@material-ui/core/Link';
+
+import List from './List';
+
+const useStyles = makeStyles((theme) => ({
+    title: {
+        fontSize: 50,
+        margin: "50px 0",
+        textAlign: "center",
+    },
+    subtitle: {
+        fontSize: 30,
+        textAlign: "center",
+    },
+    floatButton: {
+        position: 'absolute',
+        bottom: theme.spacing(12),
+        right: theme.spacing(3),
+      }
+}));  
+
 const Home = () => {
+    const classes = useStyles();
     return (
-        <div></div>
+        <Container>
+            <h1 className={classes.title}>Bienvenido</h1>
+            <p className={classes.subtitle}>¿Que deseas realizar hoy?</p>
+            <Link      
+                component={RouterLink} 
+                variant="body2" 
+                to="/create"
+                >Crear nueva orden</Link>
+            <List />
+            <Link      
+                component={RouterLink} 
+                variant="body2" 
+                to="/create"
+                >
+                <Fab color="secondary" aria-label="add" className={classes.floatButton} >
+                    <AddIcon />
+                </Fab>
+            </Link>
+        </Container>
     )
 }
 
