6f2838917b97f6379aa67d5805cf60aa9891cf24
Oriana <oriana.nohemi22@gmail.com>
2021-05-04 22:11:40 -0500

    correccion de errores del servidor

--- server/app.js
+++ server/app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const cors = require("cors");
-const config = require("./config");
 const router = require("./network/routes");
-
+const config = require("./config");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
-app.use(cors)
-router(app)
 
-app.use(config.publicRoute, express.static("public"));
+router(app)
 
 app.listen(config.port, () => {
   console.log(`La aplicacion esta escuchando en ${config.host}:${config.port}`)
